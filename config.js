d1991536df32d9f3dd4318b6182ec2a5acb01fc8
Boom <boom@Shubhams-MacBook-Pro.local>
2022-04-30 09:15:15 +0200

    added subgraph

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-    export const contractAddress = "0xA1d788945fd4B080Ab16dC1AD12a50af087D0f8E"
+    export const contractAddress = "0x10Bbfc8340BE6eFa09753741d6b838CB495aAd4b"
     export const owner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
   
